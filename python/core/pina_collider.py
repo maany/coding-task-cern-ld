92a018845c0bcafd16d93167c931cac564889727
maany <imptodefeat@gmail.com>
2021-06-27 22:31:55 +0200

    pina collider

--- python/core/pina_collider.py
+++ python/core/pina_collider.py
@@ -1,29 +1,40 @@
 import operator
 
+
 def append_index_to_mountains(mountains):
     for idx, mountain in enumerate(mountains, start=1):
-        mountain['id'] = idx
+        mountain["id"] = idx
     return mountains
 
-def sort_mountains(mountains, left_index = True):
+
+def sort_mountains(mountains, left_index=True):
     if left_index:
-        key = lambda k: k['left']
+        key = lambda k: k["left"]
     else:
-        key = lambda k: k['right']
+        key = lambda k: k["right"]
 
     sorted_mountains = sorted(mountains, key=key)
     return sorted_mountains
+
+
 def rename_key(mountain, left=False):
     if left:
-        mountain['val'] = mountain.pop('left')
+        mountain["val"] = mountain.pop("left")
     else:
-        mountain['val'] = mountain.pop('right')
+        mountain["val"] = mountain.pop("right")
+
 
 def get_x_axis(indexed_mountains):
-    left_sorted_mountains = [('left', mountain['id'], mountain['left']) for mountain in sort_mountains(indexed_mountains, left_index=True)]
-    right_sorted_mountains = [('right', mountain['id'], mountain['right']) for mountain in sort_mountains(indexed_mountains, left_index=False)]
+    left_sorted_mountains = [
+        ("left", mountain["id"], mountain["left"])
+        for mountain in sort_mountains(indexed_mountains, left_index=True)
+    ]
+    right_sorted_mountains = [
+        ("right", mountain["id"], mountain["right"])
+        for mountain in sort_mountains(indexed_mountains, left_index=False)
+    ]
     x_axis = left_sorted_mountains + right_sorted_mountains
-    x_axis.sort(key = lambda x: x[2]) 
+    x_axis.sort(key=lambda x: x[2])
     return x_axis
 
 
@@ -39,7 +50,7 @@ def pina_collider(x_axis_tuples):
         orientation, idx, val = elem
         if super_peak is None:
             super_peak = idx
-        if orientation == 'left':
+        if orientation == "left":
             open_peaks.append(idx)
         else:
             if idx in open_peaks:
@@ -49,10 +60,10 @@ def pina_collider(x_axis_tuples):
                     if len(open_peaks) > 0:
                         super_peak = open_peaks[0]
                     else:
-                        # TODO log message 
+                        # TODO log message
                         break
                 else:
                     pina_peaks.append(idx)
             else:
                 raise Exception("WTF event")
-    return [x for x in closed_peaks if x not in pina_peaks] 
+    return [x for x in closed_peaks if x not in pina_peaks]
